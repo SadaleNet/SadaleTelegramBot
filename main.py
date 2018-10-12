2b743bd22d98e4892d032b361d436b68f6df9bb7
SadaleNet <SadaleNet@users.noreply.github.com>
2018-10-12 15:58:01 +0800

    OldMessageDeleter: Implemented message deletion mechanism

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from telegramBot import TelegramBot
 from oldMessageDeleter import OldMessageDeleter
 
 if __name__ == "__main__":
-	oldMessageDeleter = OldMessageDeleter("data/oldMessageDeleter.db", 100)
-	
 	telegramBot = TelegramBot(codecs.encode(open('data/token', 'r').read().replace('\n', ''), "rot-13"))
+	oldMessageDeleter = OldMessageDeleter(telegramBot, "data/oldMessageDeleter.db", 20)
+
 	telegramBot.setLogFile('data/chat.log', logging.DEBUG)
 	telegramBot.setUpdateOffsetFilenameAndLoadOffset("data/updateOffset")
 	telegramBot.attachHook(print)
@@ -13,4 +13,5 @@ if __name__ == "__main__":
 	telegramBot.startPolling(10, 0)
 	logging.debug('STARTUP:Bot initialized!')
 	while True:
+		oldMessageDeleter.performDeleteOldMessages()
 		time.sleep(1)
